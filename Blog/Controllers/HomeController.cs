1b0be31f62c3536de13bbf05e898e3c70d86eb3b
Mir Carvalho <deppbrazil@gmail.com>
2021-06-14 23:27:44 -0300

    :rocket: Add list dynamic

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -11,27 +11,46 @@ namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            Post postOne = new Post()
+            {
+                Title = "Pirates of the Caribbean",
+                Resume = "William Turner, a resourceful young blacksmith, " +
+                "teams up with the eccentric pirate Captain Jack Sparrow " +
+                "to save his love, the Governor's daughter, Elizabeth Swann, " +
+                "who has been mistakenly captured by the clever and " +
+                "treacherous Barbossa, a former ally of Jack, to make a blood " +
+                "sacrifice so as to end the curse that has ...",
+                Category = "Action"
+            };
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            Post postTwo = new Post()
+            {
+                Title = "Avengers",
+                Resume = "S.H.I.E.L.D. has located the mysterious Tesseract " +
+                "device and the Army's super soldier Captain America. The Tesseract " +
+                "is actually a gateway to an entirely new world called Asgard. " +
+                "A mysterious being known as Loki arrives on earth and immediately " +
+                "assumes that he can rule all human beings. But that irks S.H.I.E.L.D. " +
+                "director Nick Fury the wrong way. As Loki escapes with the Tesseract, " +
+                "Nick Fury believes this is an act of war against Earth. His only hope " +
+                "is to assemble an actual team of super heroes. Dr. Bruce Banner, " +
+                "who turns into an enormous green rage monster known as the Hulk. " +
+                "Tony Stark and his venerable Iron Man armor. Captain America, " +
+                "the Stark Enterprises created super soldier. Thor, the god of thunder, " +
+                "protector of Earth and his home planet of Asgard, and Loki's brother. " +
+                "Master assassins Hawkeye and Natasha Romanoff. Together they will become " +
+                "a team to take on an attack that will call them to become the greatest of all time.",
+                Category = "Action"
+            };
+
+            Post[] posts = new Post[] { postOne, postTwo };
+            
+            ViewBag.Posts = posts;
+            
+            return View();
+        }        
     }
 }
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Resume { get; set; }
+        public string Category { get; set; }
+    }
+}
