d08366cbfb5a2522a8e5b3a921d3ab02992abd84
Mir Carvalho <deppbrazil@gmail.com>
2021-06-25 22:47:01 -0300

    :bug: Solve edit post

--- Blog/SQLScripts/Scripts.Posts.sql
+++ Blog/SQLScripts/Scripts.Posts.sql
@@ -15,11 +15,10 @@ CREATE TABLE [dbo].[Posts] (
     [Category] VARCHAR (50)  NULL
 );
 
-
 SELECT * FROM dbo.Posts;
 
 DELETE FROM dbo.Posts
-  WHERE Id = '3002';
+  WHERE Id = '11008';
 
 INSERT INTO dbo.Posts
 	VALUES  ('Piratas do Caribe 5','O capitão Salazar é a nova pedra no sapato do capitão Jack Sparrow. Ele lidera um exército de piratas fantasmas assassinos e está disposto a matar todos os piratas existentes na face da Terra. Para escapar, Sparrow precisa encontrar o Tridente de Poseidon, que dá ao seu dono o poder de controlar o mar.','Ação, Aventura');
@@ -29,3 +28,6 @@ INSERT INTO dbo.Posts
 
 INSERT INTO dbo.Posts
 	VALUES  ('O Grande Gatsby','Na primavera de 1922, Nick Carraway chega a Nova York e vira vizinho do misterioso e festeiro milionário Jay Gatsby quando vai viver do outro lado da baía com sua prima Daisy e seu marido mulherengo Tom Buchanan. Assim, Nick é atraído para o mundo cativante dos ricos, suas ilusões, amores e fraudes. Ao testemunhar fatos dentro e fora do mundo em que habita, Nick escreve um conto de amor impossível, sonhos e tragédias que espelham conflitos em tempos modernos.','Romance, Drama');
+
+INSERT INTO dbo.Posts
+	VALUES  ('Teste','teste','Romance, Drama');
